6154df538ebd44e6372936e8b23e88cc9cbd2bb5
subin kim <1008sb354@gmail.com>
2025-06-30 19:13:48 +0900

    feat: supabase env 값 없을 경우 처리

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Environment variable NEXT_PUBLIC_SUPABASE_URL is missing.");
+}
+if (!supabaseAnonKey) {
+  throw new Error(
+    "Environment variable NEXT_PUBLIC_SUPABASE_ANON_KEY is missing."
+  );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
