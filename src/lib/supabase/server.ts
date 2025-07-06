24612597f932534e5c9fbd216abfe2ac4ea074ac
subin kim <1008sb354@gmail.com>
2025-07-06 16:53:25 +0900

    콘솔 삭제

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,8 +6,6 @@ import { cookies } from "next/headers";
 export async function createSupabaseServerClient() {
   const cookieStore = await cookies();
 
-  console.log("cookieStore", cookieStore);
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
