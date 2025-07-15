f872858e0fca398348e7526cdafa0f9a0d891077
subin kim <1008sb354@gmail.com>
2025-07-15 20:50:19 +0900

    feat: loading 추가, 주소 설정 페이지 적용

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,6 +14,7 @@ import {
   getUserLinks,
   getUserWorks,
 } from "@/feature/user/api.server";
+import Loading from "../loading";
 
 export default async function ProfilePage() {
   const supabase = await createSupabaseServerClient();
