342aad85aa3032dea33fbfc52cfbfbd126c53de3
subin kim <90506668+soob1008@users.noreply.github.com>
2025-07-24 13:59:17 +0900

    feat: 어드민 헤더 디자인 변경 (#31)

--- src/feature/admin/design/PageDesignEditor.tsx
+++ src/feature/admin/design/PageDesignEditor.tsx
@@ -7,7 +7,7 @@ export default function PageDesignEditor() {
   return (
     <article className="pr-2 flex flex-col gap-4 pt-4 pl-10 pb-24 max-w-[900px] w-full mx-auto lg:max-w-none">
       <div className="flex items-center justify-between pb-2">
-        <h2 className="text-xl font-gong font-medium">꾸미기</h2>
+        <h2 className="text-lg font-bold">꾸미기</h2>
         <div className="flex items-center gap-2">
           <Button type="button" className="w-fit">
             저장하기
