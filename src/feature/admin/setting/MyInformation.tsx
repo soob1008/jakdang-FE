acc67d38516ff392ad6ee86106146ac167a9f6a5
subin kim <1008sb354@gmail.com>
2025-08-01 18:13:30 +0900

    feat: 필명 추가 및 API 연동

--- src/feature/admin/setting/MyInformation.tsx
+++ src/feature/admin/setting/MyInformation.tsx
@@ -16,14 +16,18 @@ import {
 import { Author } from "@/feature/user/type";
 import { handleAction } from "@/lib/api/action";
 import { apiClient } from "@/lib/api/api.client";
+import { Settings } from "lucide-react";
+import { revalidateUserInfo } from "../action";
 
 interface MyInformationProps {
   user: Author;
 }
 
 export default function MyInformation({ user }: MyInformationProps) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenSlugAlert, setIsOpenSlugAlert] = useState(false);
+  const [isOpenNameAlert, setIsOpenNameAlert] = useState(false);
   const [slug, setSlug] = useState(user.slug || "");
+  const [name, setName] = useState(user.display_name || "");
 
   const handleUpdateSlug = async (newSlug: string) => {
     await handleAction(
@@ -31,8 +35,22 @@ export default function MyInformation({ user }: MyInformationProps) {
       {
         successMessage: "주소가 성공적으로 업데이트되었습니다.",
         errorMessage: "주소 업데이트에 실패했습니다.",
-        onSuccess: () => {
-          setSlug(newSlug);
+        onSuccess: async () => {
+          await revalidateUserInfo();
+        },
+      }
+    );
+  };
+
+  const handleUpdateDisplayName = async (newName: string) => {
+    await handleAction(
+      () =>
+        apiClient.patch(`/api/user/display-name`, { display_name: newName }),
+      {
+        successMessage: "필명이 성공적으로 업데이트되었습니다.",
+        errorMessage: "필명 업데이트에 실패했습니다.",
+        onSuccess: async () => {
+          await revalidateUserInfo();
         },
       }
     );
@@ -45,20 +63,85 @@ export default function MyInformation({ user }: MyInformationProps) {
         <h4 className="text-sm text-gray-500">이메일</h4>
         <p className="mt-2 font-semibold">{user.email}</p>
       </div>
+      {/* 필명 */}
       <div className="border-t pt-4">
         <div className="flex items-center justify-between">
-          <h4 className="text-sm text-gray-500">주소</h4>
+          <h4 className="text-sm text-gray-500">필명</h4>
 
-          <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+          <AlertDialog open={isOpenNameAlert} onOpenChange={setIsOpenNameAlert}>
             <AlertDialogTrigger asChild>
-              <Button type="button" variant="outline" size="sm">
-                주소 수정
+              <Button
+                type="button"
+                variant="outline"
+                size="sm"
+                aria-label="필명 설정"
+              >
+                <Settings className="h-4 w-4" />
               </Button>
             </AlertDialogTrigger>
 
             <AlertDialogContent>
               <AlertDialogHeader>
-                <AlertDialogTitle>주소를 수정하시겠습니까?</AlertDialogTitle>
+                <AlertDialogTitle>필명 설정</AlertDialogTitle>
+              </AlertDialogHeader>
+
+              <div className="py-2">
+                <Input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="필명을 작성해주세요."
+                />
+              </div>
+
+              <AlertDialogFooter>
+                <AlertDialogCancel
+                  onClick={() => {
+                    setIsOpenNameAlert(false);
+                    setName(user.display_name || "");
+                  }}
+                >
+                  취소
+                </AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={async () => {
+                    await handleUpdateDisplayName(name);
+                    setIsOpenNameAlert(false);
+                  }}
+                >
+                  저장
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        {user.display_name ? (
+          <span className="font-semibold">{user.display_name}</span>
+        ) : (
+          <span className="text-sm text-gray-500">
+            아직 필명이 없어요. 지금 설정해보세요!
+          </span>
+        )}
+      </div>
+      {/* 주소 */}
+      <div className="border-t pt-4">
+        <div className="flex items-center justify-between">
+          <h4 className="text-sm text-gray-500">주소</h4>
+
+          <AlertDialog open={isOpenSlugAlert} onOpenChange={setIsOpenSlugAlert}>
+            <AlertDialogTrigger asChild>
+              <Button
+                type="button"
+                variant="outline"
+                size="sm"
+                aria-label="주소 설정"
+              >
+                <Settings className="h-4 w-4" />
+              </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>주소 설정</AlertDialogTitle>
               </AlertDialogHeader>
 
               <div className="py-2">
@@ -72,7 +155,7 @@ export default function MyInformation({ user }: MyInformationProps) {
               <AlertDialogFooter>
                 <AlertDialogCancel
                   onClick={() => {
-                    setIsOpen(false);
+                    setIsOpenSlugAlert(false);
                     setSlug(user.slug || "");
                   }}
                 >
@@ -81,7 +164,7 @@ export default function MyInformation({ user }: MyInformationProps) {
                 <AlertDialogAction
                   onClick={async () => {
                     await handleUpdateSlug(slug);
-                    setIsOpen(false);
+                    setIsOpenSlugAlert(false);
                   }}
                 >
                   저장
@@ -91,7 +174,9 @@ export default function MyInformation({ user }: MyInformationProps) {
           </AlertDialog>
         </div>
         <p className="mt-2 font-semibold">
-          {slug ? `https://jakdang.site/@${slug}` : "사용자 주소가 없습니다."}
+          {user.slug
+            ? `https://jakdang.site/@${user.slug}`
+            : "사용자 주소가 없습니다."}
         </p>
       </div>
     </div>
