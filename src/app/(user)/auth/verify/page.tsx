1ba27c17d7049025190fd2c41c0e800709a293ed
subin kim <1008sb354@gmail.com>
2025-09-28 16:41:50 +0900

    feat: 로그인 토큰 검증 로직 추가

--- src/app/(user)/auth/verify/page.tsx
+++ src/app/(user)/auth/verify/page.tsx
@@ -1,30 +1,38 @@
 "use client";
 
 import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
+import { fetchAPI } from "@/shared/lib/api/api.server";
 
 export default function VerifyPage() {
   const searchParams = useSearchParams();
+  const router = useRouter();
+  const token = searchParams.get("token");
 
   useEffect(() => {
-    const token = searchParams.get("token");
-
     if (!token) return;
 
-    (async () => {
+    const verifyToken = async () => {
       try {
-        const res = await fetch(`/auth/verify?token=${token}`);
+        const res = await fetchAPI<{ user: string; is_new_user: boolean }>(
+          `/auth/verify?token=${token}`
+        );
 
-        console.log(res);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          console.error("Error during verification:", error.message);
-        } else {
-          console.error("Unknown error:", error);
-        }
+        if (!res.user) return;
+
+        router.push(res.is_new_user ? "/auth/set-slug" : "/admin/compose");
+      } catch (err) {
+        const message = err instanceof Error ? err.message : "알 수 없는 오류";
+        console.error("Error during verification:", message);
       }
-    })();
-  }, []);
+    };
 
-  return <div>토큰: {searchParams.get("token")}</div>;
+    verifyToken();
+  }, [token, router]);
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p>로그인 확인 중...</p>
+    </div>
+  );
 }
