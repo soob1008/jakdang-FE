73c16b260bc06bfdd01ff66b1272222e985e75b3
subin kim <1008sb354@gmail.com>
2025-09-28 15:10:23 +0900

    feat: token 검증 페이지 추가

--- src/app/(user)/auth/verify/page.tsx
+++ src/app/(user)/auth/verify/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
 export default function VerifyPage() {
-  return <div>토큰</div>;
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+
+    if (!token) return;
+
+    (async () => {
+      try {
+        const res = await fetch(`/auth/verify?token=${token}`);
+
+        console.log(res);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("Error during verification:", error.message);
+        } else {
+          console.error("Unknown error:", error);
+        }
+      }
+    })();
+  }, []);
+
+  return <div>토큰: {searchParams.get("token")}</div>;
 }
