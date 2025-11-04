e9725e8a717deace082dcedd32a4b2c197b5cf40
subin kim <1008sb354@gmail.com>
2025-11-04 10:55:01 +0900

    feat: 타이틀 적용

--- src/feature/admin/design/PageDesignEditor.tsx
+++ src/feature/admin/design/PageDesignEditor.tsx
@@ -5,6 +5,7 @@ import PageCustomizer from "./PageCustomizer";
 import { useFormContext } from "react-hook-form";
 import useAutoSaveDesign from "@/feature/page/hooks/useAutoSaveDesign";
 import useUpdatePublished from "@/feature/page/hooks/useUpdatePublished";
+import Title from "@/feature/admin/components/Title";
 
 export default function PageDesignEditor() {
   const { watch } = useFormContext();
@@ -25,15 +26,16 @@ export default function PageDesignEditor() {
   };
 
   return (
-    <article className="px-4 flex flex-col gap-4 pt-4 pb-24 max-w-[900px] w-full mx-auto md:pl-10 lg:max-w-none">
-      <div className="flex items-center justify-between pb-2">
-        <h2 className="font-bold">꾸미기</h2>
-        <div className="flex items-center gap-2">
+    <article className="flex flex-col gap-4 pb-24 max-w-[900px] w-full mx-auto lg:max-w-none">
+      <Title
+        title="꾸미기"
+        rightContent={
           <Button type="button" className="w-fit" onClick={handleSavePage}>
             반영하기
           </Button>
-        </div>
-      </div>
+        }
+      />
+
       <div className="space-y-6">
         <PageCustomizer />
       </div>
